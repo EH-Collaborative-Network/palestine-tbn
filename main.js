4276066fdf471fc6e094872a3b8c24997650ddc6
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2024-08-26 17:15:44 -0400

    playing with colors/thumbnails

--- main.js
+++ main.js
@@ -44,103 +44,103 @@ resizeBG()
 
 
 
-let graphics;
-let num = 1;
-let shouldDraw = true;
-let veins = []
-function setup() {
-    let c;
+// let graphics;
+// let num = 1;
+// let shouldDraw = true;
+// let veins = []
+// function setup() {
+//     let c;
         
-        if(typeof window != `undefined`){
-            if(window.innerWidth <= 768){
-              c = createCanvas((window.innerWidth), (window.innerHeight)); 
-            }else{
-              c = createCanvas((window.innerWidth), (window.innerHeight)); 
-            }
-        }
-        c.parent('canvas')
-        background("#000000")
-        noStroke();
-    }
+//         if(typeof window != `undefined`){
+//             if(window.innerWidth <= 768){
+//               c = createCanvas((window.innerWidth), (window.innerHeight)); 
+//             }else{
+//               c = createCanvas((window.innerWidth), (window.innerHeight)); 
+//             }
+//         }
+//         c.parent('canvas')
+//         background("#000000")
+//         noStroke();
+//     }
     
 
-    function draw() {
-        if(typeof window != `undefined`){
-            if(shouldDraw){
-                for(let i = 0; i < veins.length;i++){
-                veins[i].show()
-                }     
-            }
-        }     
-    }
+//     function draw() {
+//         if(typeof window != `undefined`){
+//             if(shouldDraw){
+//                 for(let i = 0; i < veins.length;i++){
+//                 veins[i].show()
+//                 }     
+//             }
+//         }     
+//     }
 
 
-    function mouseMoved(){
-        if(random([true,false,false])){
+//     function mouseMoved(){
+//         if(random([true,false,false])){
                 
-                let x = random(0, windowWidth/4);
-                let y = random(0, windowHeight);
-                if(random([true,false])){
-                    x = random((windowWidth/4 * 3), windowWidth);
-                }
+//                 let x = random(0, windowWidth/4);
+//                 let y = random(0, windowHeight);
+//                 if(random([true,false])){
+//                     x = random((windowWidth/4 * 3), windowWidth);
+//                 }
 
-                veins.push(new Vein(x,y))
+//                 veins.push(new Vein(x,y))
             
-          }
-    }
-    function touchMoved(){
-        if(random([true,false,false])){
+//           }
+//     }
+//     function touchMoved(){
+//         if(random([true,false,false])){
                 
-                let x = random(0, windowWidth/4);
-                let y = random(0, windowHeight);
-                if(random([true,false])){
-                    x = random((windowWidth/4 * 3), windowWidth);
-                }
+//                 let x = random(0, windowWidth/4);
+//                 let y = random(0, windowHeight);
+//                 if(random([true,false])){
+//                     x = random((windowWidth/4 * 3), windowWidth);
+//                 }
 
-                veins.push(new Vein(x,y))
+//                 veins.push(new Vein(x,y))
             
-          }
-    }
-    function windowResized() {
-        if(window?.innerWidth <= 768){
-            resizeCanvas((window?.innerWidth), (window?.innerHeight));
-        }else{
-            resizeCanvas((window?.innerWidth), (window?.innerHeight));    
-        }
-        blendMode(BLEND);
-        background("#00fff")
-    }
-    class Vein{
-        constructor(x,y){
-          this.x = x;
-          this.y = y;
-          this.count = 0;
-        }
-        show(){
-            if(typeof window != `undefined`){
-            let fw = windowWidth/50;
-            let fh = windowHeight/50;
-            let xf = floor(map(this.x,0, windowWidth, 0, 50));
-            let yf = floor(map(this.y,0, windowHeight, 0, 50));
-            xf = xf + floor(random(-2,2))
-            yf = yf + floor(random(-2,2))
-          if(this.count == 0){
-                this.x = fw * xf;
-                this.y = fh * yf;
+//           }
+//     }
+//     function windowResized() {
+//         if(window?.innerWidth <= 768){
+//             resizeCanvas((window?.innerWidth), (window?.innerHeight));
+//         }else{
+//             resizeCanvas((window?.innerWidth), (window?.innerHeight));    
+//         }
+//         blendMode(BLEND);
+//         background("#00fff")
+//     }
+//     class Vein{
+//         constructor(x,y){
+//           this.x = x;
+//           this.y = y;
+//           this.count = 0;
+//         }
+//         show(){
+//             if(typeof window != `undefined`){
+//             let fw = windowWidth/100;
+//             let fh = windowHeight/100;
+//             let xf = floor(map(this.x,0, windowWidth, 0, 100));
+//             let yf = floor(map(this.y,0, windowHeight, 0, 100));
+//             xf = xf + floor(random(-2,2))
+//             yf = yf + floor(random(-2,2))
+//           if(this.count == 0){
+//                 this.x = fw * xf;
+//                 this.y = fh * yf;
             
-            rect(this.x,this.y, fw, fh);
-          }else if(this.count < 50 ){
-            if(random([true,false,false])){
-                this.x = this.x + (fw * floor(random(-2,2)));
-                this.y = this.y + (fh * floor(random(-2,2)));
-            }
-            fill(random(["#fff","#000000","#9A0000"]));
-            rect(this.x,this.y, fw, fh);
-          }
-          this.count = this.count + 1
-            }
-        }
-      }
+//             rect(this.x,this.y, fw, fh);
+//           }else if(this.count < 100 ){
+//             if(random([true,false,false])){
+//                 this.x = this.x + (fw * floor(random(-2,2)));
+//                 this.y = this.y + (fh * floor(random(-2,2)));
+//             }
+//             fill(random(["#fff","#000000","#9A0000"]));
+//             rect(this.x,this.y, fw, fh);
+//           }
+//           this.count = this.count + 1
+//             }
+//         }
+//       }
 
 
   
